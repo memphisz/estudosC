de5fe7cfdf9aa155701e17301c64afce87b20e3c
memphisz <brendinha_rebeca@hotmail.com>
2023-08-22 00:23:35 -0300

    primeiro contato com a linguagem

---
+++ helloworld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    o uso do #include <stdio.h> e #include <stdlib.h>
+    no inicio do código c serve para incluir as bibliotecas padrão do C.
+    Essas bibliotecas fornecem várias funções e declarações
+    que são essenciais para a programação em C.
+
+*/
+
+/*Main Function: devo usar parênteses vazios
+pois indica que a função "main" n recebe argumentos
+*/
+int main() {
+    printf("Hello World!");
+    // Adicionar ponto e virgula ao fim de cada instrução para indicar o fim.
+    return 0;
+}
+
