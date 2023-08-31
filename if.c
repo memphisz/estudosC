f0a8fc23e7050f42e8eb2757f31e666d585f515a
memphisz <brendinha_rebeca@hotmail.com>
2023-08-30 21:24:43 -0300

    comparando maior menor com ifs

--- if.c
+++ if.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>>
+#include <stdlib.h>
 
-int main(){
+int main() {
     int num;
     printf("Digite um numero aleatorio: ");
     scanf("%d", &num);
 
-    if(num >= 10){
-        printf("O numero eh maior ou eh 10");
+    if (num > 10) {
+        printf("O numero eh maior que 10\n");
+    } else if (num == 10) {
+        printf("O numero eh 10\n");
+    } else {
+        printf("O numero eh menor que 10\n");
     }
-    else
-        printf("O numero eh menor que 10");
-
 
     return 0;
-
-};
+}
 
 
