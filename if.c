c2368d2d41a7b585af054344cbcd3fa85051a50a
memphisz <brendinha_rebeca@hotmail.com>
2023-08-30 21:09:12 -0300

    comparando se um numero é maior ou igual a 10

---
+++ if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>>
+
+int main(){
+    int num;
+    printf("Digite um numero aleatorio: ");
+    scanf("%d", &num);
+
+    if(num >= 10){
+        printf("O numero eh maior ou eh 10");
+    }
+    else
+        printf("O numero eh menor que 10");
+
+
+    return 0;
+
+};
+
+
