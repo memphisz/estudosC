cf0292006ffaec483374ac5bb5f07a103ccfce90
memphisz <brendinha_rebeca@hotmail.com>
2023-09-04 19:39:25 -0300

    uso do for em c

---
+++ for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int cont;
+    //Laço de repetição For
+    // 1 contador 2 o limite da repetição 3 encremento(quanto eu adiciono a cada repetição)
+    for(cont = 1; cont <= 10; cont ++){
+        printf("%d - Hello World!\n", cont);
+    }
+
+    return 0;
+
+}
